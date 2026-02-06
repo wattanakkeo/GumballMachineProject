ab666d639932f41344fecc400598911523aa2ad2
wattanakkeo <wattanakkeo@gmail.com>
2026-02-06 00:18:50 -0800

    Wrote test cases for dispensing gumballs

--- src/test/java/GumballMachineTest.java
+++ src/test/java/GumballMachineTest.java
@@ -46,4 +46,48 @@ public class GumballMachineTest {
         assertEquals(55, gm.insertCoin(55));
         assertEquals(0, gm.currency.getBalance());
     }
+
+    // Test if no gumballs dispensed when user has an insufficient balance
+    @Test
+    void dispenseGumball_insufficient_balance_returnCurrentGumCount() {
+        GumballMachine gm = new GumballMachine();
+
+        // Insufficient balance, gumball dispensed amount remains the same
+        assertFalse(gm.dispenseRed());
+        assertEquals(0, gm.getRedDispensed());
+
+        assertFalse(gm.dispenseYellow());
+        assertEquals(0, gm.getYellowDispensed());
+    }
+
+    // Test if correct amount of gumballs is dispensed and balance is deducted
+    @Test
+    void dispenseGumball_sufficient_balance_incrementGumball_deductBalance_andReturnCurrentGumAmount() {
+        GumballMachine gm = new GumballMachine();
+        gm.insertCoin(Currency.QUARTER);
+
+        // Sufficient balance, deduct balance and return gumball
+        assertTrue(gm.dispenseRed());
+        assertEquals(20, gm.getBalance());
+
+        assertTrue(gm.dispenseYellow());
+        assertEquals(10, gm.getBalance());
+    }
+
+    // Test the correct amount of change dispensed when returning change
+    @Test
+    void returnChange() {
+        GumballMachine gm = new GumballMachine();
+
+        // Returns users balance
+        gm.insertCoin(1);
+        assertEquals(1, gm.pullCoinReturn());
+
+        gm.insertCoin(Currency.DIME);
+        assertEquals(10, gm.pullCoinReturn());
+
+        gm.insertCoin(Currency.NICKEL);
+        assertEquals(5, gm.pullCoinReturn());
+
+    }
 }
